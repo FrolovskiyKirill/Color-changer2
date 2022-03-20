9845a0e5414c239a4f5918a24cdbb4f014a133bf
Kirill <frolkv@gmail.com>
2022-03-20 13:47:31 +0300

    v 2.0 alpha

--- Color changer/ViewController.swift
+++ Color changer/ViewController.swift
@@ -22,26 +22,63 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redChanger.value = 0
-        greenChanger.value = 0
-        blueChanger.value = 0
-        
-        redLabel.text = "\(0.0)"
-        greenLabel.text = "\(0.0)"
-        blueLabel.text = "\(0.0)"
-        
         colorView.layer.cornerRadius = 10
+        
+        redChanger.minimumTrackTintColor = .red
+        greenChanger.minimumTrackTintColor = .green
+        
+        setColor()
+        setValue(for: redLabel, greenLabel, blueLabel)
     }
     
-    @IBAction func sliderRed(_ sender: Any) {
-        
-        colorView.backgroundColor = UIColor(red: CGFloat(redChanger.value), green: CGFloat(greenChanger.value), blue: CGFloat(blueChanger.value), alpha: 1)
-        
-        redLabel.text = "\(round((100 * redChanger.value)) / 100)"
-        greenLabel.text = "\(round((100 * greenChanger.value)) / 100)"
-        blueLabel.text = "\(round((100 * blueChanger.value)) / 100)"
-        
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+        switch sender {
+        case redChanger:
+            redLabel.text = string(from: redChanger)
+        case greenChanger:
+            greenLabel.text = string(from: greenChanger)
+        default:
+            blueLabel.text = string(from: blueChanger)
+        }
     }
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redChanger.value),
+            green: CGFloat(greenChanger.value),
+            blue: CGFloat(blueChanger.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redLabel:
+                redLabel.text = string(from: redChanger)
+            case greenLabel:
+                greenLabel.text = string(from: greenChanger)
+            default:
+                blueLabel.text = string(from: blueChanger)
+            }
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+//    @IBAction func sliderRed(_ sender: Any) {
+//
+//        colorView.backgroundColor = UIColor(red: CGFloat(redChanger.value),
+//                                            green: CGFloat(greenChanger.value),
+//                                            blue: CGFloat(blueChanger.value), alpha: 1)
+//
+//        redLabel.text = "\(round((100 * redChanger.value)) / 100)"
+//        greenLabel.text = "\(round((100 * greenChanger.value)) / 100)"
+//        blueLabel.text = "\(round((100 * blueChanger.value)) / 100)"
+//
+//    }
     
 }
 
